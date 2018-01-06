b57d7379c6bac8c55d90679f3405cf13f2e52f05
Simon Lang <simon.lang@cambia.org>
2018-01-07 08:30:56 +1000

    Delete all unused boilerplate
    Load in only used bootstrap styles

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,7 @@ import Vue from 'vue';
 import VueRouter, { Location, Route, RouteConfig } from 'vue-router';
 import { makeHot, reload } from './util/hot-reload';
 
-const homeComponent = () => import('./components/home').then(({ HomeComponent }) => HomeComponent);
 const listComponent = () => import('./components/list').then(({ ListComponent }) => ListComponent);
-// const homeComponent = () => import(/* webpackChunkName: 'home' */'./components/home').then(({ HomeComponent }) => HomeComponent);
-// const aboutComponent = () => import(/* webpackChunkName: 'about' */'./components/about').then(({ AboutComponent }) => AboutComponent);
-// const listComponent = () => import(/* webpackChunkName: 'list' */'./components/list').then(({ ListComponent }) => ListComponent);
 
 if (process.env.ENV === 'development' && module.hot) {
   const homeModuleId = './components/home';
@@ -14,9 +10,6 @@ if (process.env.ENV === 'development' && module.hot) {
 
   // first arguments for `module.hot.accept` and `require` methods have to be static strings
   // see https://github.com/webpack/webpack/issues/5668
-  makeHot(homeModuleId, homeComponent,
-    module.hot.accept('./components/home', () => reload(homeModuleId, (<any>require('./components/home')).HomeComponent)));
-
   makeHot(listModuleId, listComponent,
     module.hot.accept('./components/list', () => reload(listModuleId, (<any>require('./components/list')).ListComponent)));
 }
@@ -26,12 +19,8 @@ Vue.use(VueRouter);
 export const createRoutes: () => RouteConfig[] = () => [
   {
     path: '/',
-    component: homeComponent,
-  },
-  {
-    path: '/list',
     component: listComponent,
-  }
+  },
 ];
 
 export const createRouter = () => new VueRouter({ mode: 'history', routes: createRoutes() });
