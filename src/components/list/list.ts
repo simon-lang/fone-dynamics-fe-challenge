80704a022411a7df51bd0b8d97c421bd837d27f3
Simon Lang <simon.lang@cambia.org>
2018-01-06 11:41:33 +1000

    Implement core list search/select functionality
    No tests or styling consideration yet

--- src/components/list/list.ts
+++ src/components/list/list.ts
@@ -1,6 +1,8 @@
 import { Component, Vue } from 'vue-property-decorator';
 import axios, { AxiosResponse } from 'axios';
 
+import './list.scss';
+
 interface ClientAccount {
     AccountId: number;
     AccountName: string;
@@ -10,6 +12,7 @@ interface ClientAccount {
 interface ClientProperty {
     PropertyId: number;
     Name: string;
+    Account: ClientAccount;
 }
 
 @Component({
@@ -17,11 +20,13 @@ interface ClientProperty {
 })
 export class ListComponent extends Vue {
 
-    accounts: ClientAccount[] = [];
-    account: ClientAccount;
-    properties: ClientProperty[] = [];
-    term: string;
-    private url = '/assets/data.json';
+    private _accounts: ClientAccount[] = [];
+    private accounts: ClientAccount[] = [];
+    private selectedAccount: ClientAccount = null;
+    private properties: ClientProperty[] = [];
+    private alert: string = '';
+    private term: string = '';
+    private url: string = '/assets/data.json';
     protected axios;
 
     constructor() {
@@ -36,18 +41,56 @@ export class ListComponent extends Vue {
     }
 
     search() {
-        // console.log(this.term);
+        const term = this.term.toLowerCase();
+        this.selectedAccount = null;
+        this.properties = [];
+        // Search all properties
+        this.accounts = this._accounts.filter(account => {
+            const properties = account.Properties.filter(property => property.Name.toLowerCase().includes(term));
+            this.properties = this.properties.concat(properties);
+            return properties.length > 0;
+        });
+        // If no property matches, search accounts
+        if (this.accounts.length === 0) {
+            this.accounts = this._accounts.filter(account => account.AccountName.toLowerCase().includes(term));
+            if (this.accounts.length > 0) {
+                // Select first account
+                this.selectAccount(this.accounts[0]);
+            }
+        }
     }
 
-    selectAccount(account) {
-        this.account = account;
+    clearTerm() {
+        this.term = '';
+        this.properties = [];
+        this.search();
+    }
+
+    selectAccount(account: ClientAccount) {
+        this.selectedAccount = account;
         this.properties = account.Properties;
+    }
+
+    selectProperty(property: ClientProperty) {
+        this.alert = `Clicked ${property.Account.AccountName} - ${property.Name}`;
+        this.$emit('ClientPropertySelected', property.Account, property);
+    }
+
+    clearAlert() {
+        this.alert = '';
     }
 
     private loadItems() {
         if (!this.accounts.length) {
             this.axios.get(this.url).then((response: AxiosResponse) => {
-                this.accounts = response.data;
+                // Map Properties back to parent Accounts for later
+                response.data.forEach(account => {
+                    account.Properties.forEach(property => {
+                        property.Account = account;
+                    });
+                });
+                this._accounts = response.data;
+                this.search();
             }, (error) => {
                 console.error(error);
             });
